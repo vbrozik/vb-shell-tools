9f3c726c02959f0ee0faf6b026e7fb8c88242a8c
Václav Brožík <5073939+vbrozik@users.noreply.github.com>
2026-08-11 23:40:09 +0200

    refactor(net_diag_snippets): Generalize ARP scan

--- fragments/net_diag_snippets.sh
+++ fragments/net_diag_snippets.sh
@@ -4,13 +4,32 @@
 
 exit 0
 # shellcheck disable=SC2317  # The file is not to be executed.
+if false ; then
 
 
-# ARP scan using arping
+# ARP-scan a C network on a given interface
+# alternative: sh -c ... $(seq 1 254) ...
 
-sh -c '
-    for octet4 in $(seq 1 254) ; do
-        arping -c1 -I bond2.3380 10.127.50.$octet4 |
-            grep -F "reply from" &
-            sleep 0.005
-    done'
+interface=bond5.123
+ip_prefix=10.11.12
+bash -c "
+    for octet4 in {1..254} ; do
+        arping -c1 -I '$interface' '$ip_prefix'\$octet4 |
+            grep -F 'reply from' &
+        sleep 0.005
+    done"
+
+
+interface=bond5.123
+ip_prefix=10.11.12
+ip neigh flush dev "$interface"
+bash -c "
+    for octet4 in {1..254} ; do
+        ping -c1 '$ip_prefix'\$octet4 | grep -F ' from ' &
+        sleep 0.005
+    done"
+ip neigh list dev "$interface" |
+    grep -Ev '(FAILED|INCOMPLETE)$' | sort
+
+
+fi
